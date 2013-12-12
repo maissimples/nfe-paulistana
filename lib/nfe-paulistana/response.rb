ef186e57f7d6f930cafdf12848903f1878329c51
Felipe Navas <felipenavas@runrun.it>
2013-12-12 20:17:57 -0200

    Nori parser fix

--- lib/nfe-paulistana/response.rb
+++ lib/nfe-paulistana/response.rb
@@ -1,15 +1,13 @@
 module NfePaulistana
   class Response
-    RETURN_ROOT = {
-      teste_envio_lote_rps: :envio_lote_rps,
-      consulta_n_fe: :consulta,
-      consulta_n_fe_emitidas: :consulta,
-      consulta_n_fe_recebidas: :consulta,
-      consulta_lote: :consulta,
-      consulta_informacoes_lote: :informacoes_lote,
-    }
     def initialize(options = {})
       @options = options
+      @nori = Nori.new({
+        :parser => :nokogiri,
+        :advanced_typecasting => true,
+        :convert_tags_to => lambda { |tag| tag.snakecase.to_sym },
+        :strip_namespaces => true
+      })
     end
 
     def xml
@@ -17,7 +15,7 @@ module NfePaulistana
     end
 
     def retorno
-      Nori.parse(xml)[("retorno_" + (RETURN_ROOT[@options[:method]] || @options[:method]).to_s).to_sym]
+      @retorno ||= @nori.parse(xml).first[1]
     end
 
     def success?
